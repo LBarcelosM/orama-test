520c12ca59f2678e5fcfae8e1090b7f50523aed0
LBarcelosM <lbarcelosm@hotmail.com>
2021-07-08 22:54:15 -0300

    Melhorias no layout

--- app/src/main/java/com/lbarcelosm/oramatest/addapters/FundListAddapter.kt
+++ app/src/main/java/com/lbarcelosm/oramatest/addapters/FundListAddapter.kt
@@ -8,6 +8,11 @@ import android.widget.BaseAdapter
 import android.widget.TextView
 import com.lbarcelosm.oramatest.R
 import com.lbarcelosm.oramatest.models.Fund
+import java.math.RoundingMode
+import java.text.DecimalFormat
+import java.text.NumberFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class FundListAddapter(private val context: Context,
                        private val dataSource: ArrayList<Fund>) : BaseAdapter() {
@@ -32,6 +37,25 @@ class FundListAddapter(private val context: Context,
         val fund = getItem(position) as Fund
         val fundTitulo = rowView.findViewById(R.id.fund_titulo) as TextView
         fundTitulo.text = fund.simple_name
+        val fundPercent = rowView.findViewById(R.id.fund_percent) as TextView
+        // Como não sei qual a propriedade da % pequei uma qualquer só para exibir um valor para todos os itens
+        val valorPercent = fund.volatility_12m.toDouble() * 100
+        fundPercent.text = roundOffDecimal(valorPercent).toString() + "% (12 M)"
+        val fundValor = rowView.findViewById(R.id.fund_valor) as TextView
+        fundValor.text = getValorFormatado(fund.operability.minimum_initial_application_amount)
         return rowView
+    }
+
+    fun roundOffDecimal(number: Double): Double? {
+        val df = DecimalFormat("#.##")
+        df.roundingMode = RoundingMode.CEILING
+        return df.format(number).toDouble()
+    }
+
+    fun getValorFormatado(valor: String) : String {
+        val format = NumberFormat.getCurrencyInstance();
+        format.setMaximumFractionDigits(2);
+        format.setCurrency(Currency.getInstance("BRL"));
+        return format.format(valor.toDouble())
     }
 }
